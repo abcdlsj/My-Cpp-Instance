bc511524b17d7c48b1106983f774d69fd6475233
abcdlsj <lisongjianshuai@gmail.com>
2020-05-10 22:29:50 +0800

    update

---
+++ Data-Structures/vector.h
@@ -0,0 +1,19 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+typedef int Rank;
+#define DEFAULT_CAPACITY 3
+
+template <typename T>
+class Vector {
+ protected:
+  Rank _size;
+  int _capacity;
+  *T _elem;
+  void copyFron(T const* A, Rank low, Rank hign);
+  void expand();
+  void shrink();
+  void bubble(Rank low, Rank hign);
+}
+
+#endif //__VECTOR_H__
